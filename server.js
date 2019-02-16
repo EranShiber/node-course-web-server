70fdd2d83128100a590ee9094a30990ae06f503e
eran <eranshiber7@gmail.com>
2019-02-16 11:01:10 +0200

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const port = process.env.PORT || 3000;
 
 const app = express();
 app.set('view engine', 'hbs');
 
 
-
 app.use(express.static(__dirname + '/public'));
 app.use((req, res, next) => {
     const logs = `logs: ${req.path}, ${req.route}, at: ${new Date().getTime().toString()}`
-   fs.appendFile('server-log.txt', logs , (err) => {
-       if (err) {
-           console.log(err, 'err')
-       }
+    fs.appendFile('server-log.txt', logs, (err) => {
+        if (err) {
+            console.log(err, 'err')
+        }
     })
     next();
 })
-app.use((req, res ,next) => {
+app.use((req, res, next) => {
     res.render('maintenance.hbs')
 })
 
@@ -29,46 +29,18 @@ app.get('/about', (req, res) => {
 })
 
 app.get('/home', (req, res) => {
-    res.render('home.hbs',{
+    res.render('home.hbs', {
         pageTitle: 'Home',
         welcomeUser: 'Hello User, please sign in!'
     })
 })
 
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.render('./aboutme.hbs', {
         pageTitle: 'about Me'
     })
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, ()=> {console.log('server is up')})
+app.listen(port, () => { console.log(`server is up at ${port}`) })
