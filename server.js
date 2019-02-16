b8bbd6e3cbdecad211692bf0a3aa25e651b8d3dd
eran <eranshiber7@gmail.com>
2019-02-16 08:25:02 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+app.set('view engine', 'hbs');
+
+
+
+app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    const logs = `logs: ${req.path}, ${req.route}, at: ${new Date().getTime().toString()}`
+   fs.appendFile('server-log.txt', logs , (err) => {
+       if (err) {
+           console.log(err, 'err')
+       }
+    })
+    next();
+})
+app.use((req, res ,next) => {
+    res.render('maintenance.hbs')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    })
+})
+
+app.get('/home', (req, res) => {
+    res.render('home.hbs',{
+        pageTitle: 'Home',
+        welcomeUser: 'Hello User, please sign in!'
+    })
+})
+
+
+app.get('/', (req,res) => {
+    res.render('./aboutme.hbs', {
+        pageTitle: 'about Me'
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000, ()=> {console.log('server is up')})
